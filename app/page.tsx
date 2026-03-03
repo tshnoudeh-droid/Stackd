6d7576c3733be901fc28864eeab16e78b629d7bd
tawfic shnoudeh <tshnoudeh@icloud.com>
2026-03-03 13:27:59 -0500

    Update margin settings in AreaChart component for improved layout and visual balance in Home page.

--- app/page.tsx
+++ app/page.tsx
@@ -925,7 +925,7 @@ export default function Home() {
                 <ResponsiveContainer width="100%" height="100%">
                   <AreaChart
                     data={chartData}
-                    margin={{ top: 16, right: 20, left: 0, bottom: 0 }}
+                    margin={{ top: 36, right: 60, left: 0, bottom: 0 }}
                   >
                     <defs>
                       <linearGradient id="colorBalance" x1="0" y1="0" x2="0" y2="1">
